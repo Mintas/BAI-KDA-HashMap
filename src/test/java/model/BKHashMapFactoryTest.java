07c1df6b578154e4626dccd6407ce5d67df37be5
Mintas <mintas93@gmail.com>
2016-01-06 15:35:41 +0300

    test refactored to increase readability of "main" part

--- src/test/java/model/BKHashMapFactoryTest.java
+++ src/test/java/model/BKHashMapFactoryTest.java
@@ -22,16 +22,11 @@ public class BKHashMapFactoryTest {
 
     @Before
     public void setUp() throws Exception {
-        //hashMapFactory = BKHashMapFactory.init();
         hashMapFactory = new BKHashMapFactory(CAPACITY);
     }
 
-
-    //todo : NOTE !!! With ALL THREE naive implementations of Map  averageTime getting better as we go testing;
     @Test
     public void makeTest() throws Exception{
-        //performTest(new BKSimpleMap<>(), true);
-
         performTest(hashMapFactory.javaConcurrent());
 
         performTest(hashMapFactory.javaSynchronized());
@@ -41,48 +36,6 @@ public class BKHashMapFactoryTest {
         performTest(hashMapFactory.synchronizeed());
 
         performTest(hashMapFactory.globalLock());
-
-    }
-
-    @Ignore
-    @Test
-    public void loadingsTest() {
-        int[] loa = bucketLoadings();
-
-        int c = 0;
-        for (int i : loa) if (i!=0) c++;
-        System.out.println(c);
-        System.out.println("yo");
-    }
-
-    @Ignore
-    @Test
-    public void makeBigTest() throws Exception{
-        performBestTimeTest(hashMapFactory.synchronizeed());
-
-        performBestTimeTest(hashMapFactory.globalLock());
-
-        performBestTimeTest(hashMapFactory.fineGrained());
-    }
-
-
-    public void performBestTimeTest(final BKMap<String, Integer> bkMap) throws InterruptedException {
-        System.out.println("Test started for: " + bkMap.getClass());
-        long bestTime = 99999999;
-        long averageTime = 0;
-        int loops = 10;
-        int sucLoops = 0;
-        for (int i = 0; i < loops; i++) {
-                long thisTime = performTest(bkMap, false);
-                bestTime = min(bestTime, thisTime);
-                averageTime += thisTime;
-                sucLoops++;
-        }
-
-        if (sucLoops>0) {
-            System.out.println("\n TOTAL " + bkMap.getClass() + " the AVERAGE time is " + averageTime / sucLoops + " ms");
-            System.out.println("\n TOTAL " + bkMap.getClass() + " the BEST time is " + bestTime + " ms\n");
-        }
 
     }
 
@@ -124,9 +77,56 @@ public class BKHashMapFactoryTest {
             averageTime += totalTime;
             if (innerMsg) System.out.println((THREAD_POOL_SIZE * REMOVE_PUT_NUMBER / 1000) +  "K entries added/retrieved in " + totalTime + " ms");
         }
-        System.out.println("For " + bkMap.getClass() + " the average time is " + averageTime / loops + " ms\n");
-        System.out.println("Size is = " + bkMap.size()); //the size expected must be \approx 2*MEAN
+        System.out.println("For " + bkMap.getClass() + " the average time is " + averageTime / loops + " ms");
+        System.out.println("Size is = " + bkMap.size() + "\n \n"); //the size expected must be \approx 2*MEAN
         return averageTime/loops;
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////
+    //here goes the test to find out better value of executionTime;
+    // averaging out effect of JVM opyimizations
+    @Ignore
+    @Test
+    public void makeBigTest() throws Exception{
+        performBestTimeTest(hashMapFactory.synchronizeed());
+
+        performBestTimeTest(hashMapFactory.globalLock());
+
+        performBestTimeTest(hashMapFactory.fineGrained());
+    }
+
+    public void performBestTimeTest(final BKMap<String, Integer> bkMap) throws InterruptedException {
+        System.out.println("Test started for: " + bkMap.getClass());
+        long bestTime = Long.MAX_VALUE;
+        long averageTime = 0;
+        int loops = 10;
+        int sucLoops = 0;
+        for (int i = 0; i < loops; i++) {
+            long thisTime = performTest(bkMap, false);
+            bestTime = min(bestTime, thisTime);
+            averageTime += thisTime;
+            sucLoops++;
+        }
+
+        if (sucLoops>0) {
+            System.out.println("\n TOTAL " + bkMap.getClass() + " the AVERAGE time is " + averageTime / sucLoops + " ms");
+            System.out.println("\n TOTAL " + bkMap.getClass() + " the BEST time is " + bestTime + " ms\n");
+        }
+
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////
+    //here goes utility code to test bucketLoadings for Capacity and Mean settings;
+    // bucket-defining code is 1-by-1 as in our HashMap implementation
+    @Ignore
+    @Test
+    public void loadingsTest() {
+        int[] loa = bucketLoadings();
+
+        int c = 0;
+        for (int i : loa) if (i!=0) c++;
+        System.out.println(c);
+        System.out.println("yo");
     }
 
     public int[] bucketLoadings(){
@@ -154,7 +154,6 @@ public class BKHashMapFactoryTest {
 
     private static int fromCode(Object key) {
         int h = key.hashCode();
-        return h ^ (h >>> 16); //pow(2,4); like div(16)
-        //return key.hashCode();
+        return h ^ (h >>> 16);
     }
 }
